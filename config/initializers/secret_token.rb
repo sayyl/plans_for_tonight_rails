758d7927cba0748c883ae0f03553dc3ea4221608
BaharKK <bahar_rachapalli@yahoo.com>
2016-02-23 19:13:50 +0000

    Rails Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlansForTonightRails::Application.config.secret_key_base = 'c5a0317dff74154571624f8321fbd06a5760ea99a78a51960d6ae0e37d2ba522583de980ac48342c7ab555908c1c890b5bcb6f6a7d19b8f711f9cc27f7b2a680'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlansForTonightRails::Application.config.session_store :cookie_store, key: '_plans_for_tonight_rails_session'
