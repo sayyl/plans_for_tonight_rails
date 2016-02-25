b985b1c138f242730c39feeec91544d858c19326
sayyl <sayyl@users.noreply.github.com>
2016-02-25 22:49:57 +0000

    cleaned up validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :events
 
-  validates: :name, presence: true
-  validates: :description, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
 
 end
--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,9 +1,9 @@
 class Consumer < ActiveRecord::Base
-  has_many: transactions
+  has_many :transactions
 
-  validates: :user_name, presence: true
-  validates: :email, presence: true, uniqueness: true
-  validates: password: presence: true
+  validates :user_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
   has_secure_password 
 end
