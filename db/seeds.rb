2f6f7b46ed83a193a3cb7eb7691215a4c9001290
sayyl <sayyl@users.noreply.github.com>
2016-02-26 20:04:26 +0000

    event and transaction seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,30 +19,54 @@ Faker::Config.locale = :'en-CA'
 #   )
 # end
 
-150.times do |n|
-  corporate_id = 1
-  category_id = Category.all.sample.id
-  name = Faker::App.name
-  location = Faker::Address.city
-  description = Faker::Lorem.sentences
-  show_date = Faker::Date.between(185.days.ago, Date.today)
-  start_time = Faker::Time.between(185.days.ago, Time.now, :all)
-  duration = Faker::Number.between(60, 240)
-  image = Faker::Placeholdit.image
-  ticket_available = Faker::Number.between(50, 200)
-  Event.create!(
-    corporate_id: corporate_id,
-    category_id: category_id,
-    name: name,
-    location: location,
-    description: description,
-    show_date: show_date,
-    start_time: start_time,
-    duration: duration,
-    image: image,
-    ticket_available: ticket_available
+# 150.times do |n|
+#   corporate_id = 1
+#   category_id = Faker::Number.between(2, 4)
+#   name = Faker::App.name
+#   location = Faker::Address.city
+#   description = Faker::Lorem.sentences.join(" ")
+#   show_date = Faker::Date.between(185.days.ago, Date.today)
+#   start_time = Faker::Time.between(185.days.ago, Time.now, :all)
+#   duration = Faker::Number.between(60, 240)
+#   # remote_image_url = Faker::Placeholdit.image
+#   ticket_available = Faker::Number.between(50, 200)
+#   Event.create!(
+#     corporate_id: corporate_id,
+#     category_id: category_id,
+#     name: name,
+#     location: location,
+#     description: description,
+#     show_date: show_date,
+#     start_time: start_time,
+#     duration: duration,
+#     # remote_image_url: remote_image_url,
+#     ticket_available: ticket_available
+#   )
+# end
+
+# 500.times do |n|
+#   consumer_id = Consumer.all.sample.id
+#   total = Faker::Number.decimal(2)
+#   successful = Faker::Boolean.boolean(0.95)
+#   Transaction.create!(
+#     consumer_id: consumer_id,
+#     total: total,
+#     successful: successful
+#   )
+# end
+
+500.times do |n|
+  type_array = ["General", "Adult", "Child"]
+  event_id = Event.all.sample.id
+  transaction_id = Transaction.all.sample.id
+  price = Faker::Number.between(20, 150)
+  type = type_array.sample
+  confirmation_num = Faker::Number.number(8)
+  Ticket.create!(
+    event_id: event_id,
+    transaction_id: transaction_id,
+    price: price,
+    type: type,
+    confirmation_num: confirmation_num
   )
 end
-
-
-
