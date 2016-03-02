06ad223383e7e7005988efb23ddf2e591309dcaa
TyneBloomfield <cat.is.fite@gmail.com>
2016-03-02 14:25:23 -0800

    prepping to merge with db changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301094022) do
+ActiveRecord::Schema.define(version: 20160302084810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160301094022) do
     t.string   "remote_image_url"
     t.decimal  "general_ticket"
     t.decimal  "child_ticket"
+    t.integer  "discount"
   end
 
   add_index "events", ["category_id"], name: "index_events_on_category_id", using: :btree
