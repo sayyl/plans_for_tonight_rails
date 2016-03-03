e8263ffae5150f933141b78ea961bbb40d7f6395
sayyl <sayyl@users.noreply.github.com>
2016-03-03 00:34:07 +0000

    styled each event id

--- app/assets/javascripts/corporates.js
+++ app/assets/javascripts/corporates.js
@@ -10,5 +10,40 @@ $('#past_events').on('click', function() {
   return false; 
 });
 
-
 $('a[data-popup').on('click', function(e) {window.open($(this).attr('href'), "Popup", "height=600, width=600"); e.preventDefault(); });
+
+function htmlbodyHeightUpdate(){
+  var height3 = $( window ).height()
+  var height1 = $('.nav').height()+50
+  height2 = $('.main').height()
+  if(height2 > height3){
+    $('html').height(Math.max(height1,height3,height2)+10);
+    $('body').height(Math.max(height1,height3,height2)+10);
+  }
+  else
+  {
+    $('html').height(Math.max(height1,height3,height2));
+    $('body').height(Math.max(height1,height3,height2));
+  }
+  
+}
+$(document).ready(function () {
+  htmlbodyHeightUpdate()
+  $( window ).resize(function() {
+    htmlbodyHeightUpdate()
+  });
+  $( window ).scroll(function() {
+    height2 = $('.main').height()
+      htmlbodyHeightUpdate()
+  });
+});
+
+$('#graph1').on('click', function() {
+  $('#total_transactions').toggle();
+  $('#calendar').toggle();
+});
+
+$('#graph2').on('click', function() {
+  $('#transaction_monthly').toggle();
+  $('#calendar').toggle();
+});
